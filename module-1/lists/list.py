c4af7c7fe7bad9141d4df8dc78684771ad984744
AHMED NUR E SAFA <ahmedsafa0759@gmail.com>
2024-10-24 17:37:44 +0600

    new added

---
+++ module-1/lists/list.py
@@ -0,0 +1,16 @@
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[2:5])
+
+
+# thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+# print(thislist[-4:-1])
+
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist.insert(2, "watermelon")
+# print(thislist)
+
+
+# fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+# newlist = [x for x in fruits if "a" in x]
+# print(newlist)
